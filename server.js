1f60821723a4e51bc94a1d462e8050faa83a16be
PabloCarvajal4 <86016435+PabloCarvajal4@users.noreply.github.com>
2021-12-11 12:06:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.urlBackend);
-    }
-    next();
-}
-
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(requireHTTPS);
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/votacion'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/votacion/'}),
+    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
 );
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
